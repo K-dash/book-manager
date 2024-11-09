9532a0f5bad27492b543f90297c39a0d81582379
K <51281148+K-dash@users.noreply.github.com>
2024-11-09 19:16:27 +0900

    feat: Add health check endpoint and update dependencies
    アプリケーションにヘルスチェックエンドポイントを追加し、依存関係としてanyhow、axum、tokioを導入しました。src/main.rsファイルにヘルスチェック関数を追加し、8080ポートでのサーバー設定を実施しました。また、rstestをdev-dependenciesに追加し、ヘルスチェックのテストを実装しました。

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,27 @@
-fn main() {
-    println!("global: {}", std::env::var("GLOBAL").unwrap());
-    println!("local: {}", std::env::var("LOCAL").unwrap());
+use std::net::{Ipv4Addr, SocketAddr};
+
+use anyhow::Result;
+use axum::{http::StatusCode, routing::get, Router};
+use tokio::net::TcpListener;
+
+pub async fn health_check() -> StatusCode {
+    StatusCode::OK
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let app = Router::new().route("/health", get(health_check));
+    let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8080);
+    let listener = TcpListener::bind(addr).await?;
+
+    println!("Server running on: {}", addr);
+
+    Ok(axum::serve(listener, app).await?)
+}
+
+// test
+#[tokio::test]
+async fn test_health_check() {
+    let status_code = health_check().await;
+    assert_eq!(status_code, StatusCode::OK);
 }
